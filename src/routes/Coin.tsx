31b7324ccb18e1da1cffa65de15a07b9fe0ce4f2
onehousesilver <onehousesilver@naver.com>
2022-03-11 23:49:57 +0900

    4.4 Routes State

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useParams, useLocation } from "react-router";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+  font-size: 30px;
+`;
+
+
+interface RouteState {
+  state:{
+    name:string;
+    };
+}
+
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams();
+  const { state } = useLocation() as RouteState;
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading"}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
+}
+
+export default Coin;

fd8b4957f6a897cd57e34ed02f88141dfe111444
onehousesilver <onehousesilver@naver.com>
2022-03-13 01:00:02 +0900

    4.10 React Query part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -76,6 +77,7 @@ function App() {
     <>
       <Router />
       <GlobalStyle />
+      < ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }

3c0c5e753eb6314a5686521b1216ce9626b294bd
onehousesilver <onehousesilver@naver.com>
2022-03-13 01:37:18 +0900

    4.12 Price Char

--- src/api.ts
+++ src/api.ts
@@ -11,5 +11,15 @@ export function fetchCoinInfo(coinId: string) {
 }
 
 export function fetchCoinTikers(coinId: string) {
-  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) => response.json());
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
+    response.json()
+  );
+}
+
+export function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7 * 2
+  return fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`).then(
+    (response) => response.json()
+  );
 }
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -211,7 +211,7 @@ function Coin() {
 
           <Routes>
             <Route path="price" element={<Price />} />
-            <Route path="chart" element={<Chart />} />
+            <Route path="chart" element={<Chart coinId={coinId!}/>} />
           </Routes>
         </>
       )}
